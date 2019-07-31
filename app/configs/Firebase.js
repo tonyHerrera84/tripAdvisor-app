ddafad313610b2f9f6a7a4a3fff7eb30f6d41f6a
Antonio Aguilar Herrera <antonio.herrera@live.com>
2019-07-31 14:43:58 -0500

    Create Firebase.js

---
+++ app/configs/Firebase.js
@@ -0,0 +1,9 @@
+export default (firebaseConfig = {
+    apiKey: "AIzaSyC0KPVYU6YyfIDjOEMfHoFuca0yREUEhTs",
+    authDomain: "react-apps-6b792.firebaseapp.com",
+    databaseURL: "https://react-apps-6b792.firebaseio.com",
+    projectId: "react-apps-6b792",
+    storageBucket: "react-apps-6b792.appspot.com",
+    messagingSenderId: "776039364486",
+    appId: "1:776039364486:web:9ad2683214fdc71a"
+});
