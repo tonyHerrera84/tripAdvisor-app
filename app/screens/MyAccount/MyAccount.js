7a2d5fbd8671292994e9675f614d370a89f6ec0c
Antonio Aguilar Herrera <antonio.herrera@live.com>
2019-08-09 17:09:57 -0500

    Update MyAccount.js

--- app/screens/MyAccount/MyAccount.js
+++ app/screens/MyAccount/MyAccount.js
@@ -4,7 +4,7 @@ import { Button } from 'react-native-elements';
 
 export default class MyAccount extends Component {
     goToScreen = nameScreen => {
-        console.log(nameScreen);
+        this.props.navigation.navigate(nameScreen);
     }
 
     render() {
