9bc8787290b2fa67f89e8a25fe5f0feb6abf036d
Antonio Aguilar Herrera <antonio.herrera@live.com>
2019-08-16 16:36:37 -0500

    Update Login.js

--- app/forms/Login.js
+++ app/forms/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import t from 'tcomb-form-native';
 import formValidation from '../configs/Validations';
+import inputTemplate from './templates/input';
 
 export const LoginStruct = t.struct({
     email: formValidation.email,
@@ -11,10 +12,22 @@ export const LoginStruct = t.struct({
 export const LoginOptions = {
     fields: {
         email: {
-            placeholder: 'Escribe tu email'
+            template: inputTemplate,
+            config: {
+                placeholder: 'Escribe tu email',
+                iconType: 'material-community',
+                iconName: 'at'
+            }
         },
         password: {
-            placeholder: 'Escribe tu contraseña'
+            template: inputTemplate,
+            config: {
+                placeholder: 'Escribe tu email',
+                iconType: 'material-community',
+                iconName: 'lock-outline',
+                password: true,
+                secureTextEntry: true
+            }
         }
     }
 };
