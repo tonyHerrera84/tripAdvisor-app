8554f58d64c10d3099b29ea74b774ecc5d6592aa
Antonio Aguilar Herrera <antonio.herrera@live.com>
2019-08-16 23:32:27 -0500

    Completando login, validaciones y diseño

--- app/screens/MyAccount/Login.js
+++ app/screens/MyAccount/Login.js
@@ -7,18 +7,69 @@ const Form = t.form.Form;
 
 import {LoginStruct, LoginOptions} from '../../forms/Login';
 
+import * as firebase from 'firebase';
+
+import Toast, {DURATION} from 'react-native-easy-toast';
 export default class Login extends Component {
     constructor() {
         super();
         this.state = {
             loginStruct: LoginStruct,
-            loginOptions: LoginOptions
+            loginOptions: LoginOptions,
+            loginData:{
+                email: '',
+                password: ''
+            },
+            loginErrorMessage: ''
         };
+    };
+
+    login = () => {
+        console.log('Haciendo login');
+        console.log(this.state.loginData);
+        const validate = this.refs.loginForm.getValue();
+        console.log(validate);
+
+        if (!validate) {
+            console.log('Formulario incorrecto');
+            this.setState({
+                loginErrorMessage: 'Los datos del formulario son erroneos'
+            })
+        } else {
+            console.log('logeando');
+            this.setState({
+                loginErrorMessage: ''
+            })
+            firebase
+            .auth()
+            .signInWithEmailAndPassword(validate.email, validate.password)
+            .then(resolve => {
+                console.log('Login correcto');
+                console.log(resolve);
+                this.refs.toast.show('Login correcto.', 200, () => {
+                    this.props.navigation.goBack();
+                });
+            })
+            .catch(err => {
+                console.log('Error en el registro');
+                console.log(err);
+                this.refs.toast.show('Login incorrecto revise sus datos.', 2500);
+            })
+        }
+    };
+
+    onChangeFormLogin = (formValue) => {
+        console.log('Cahnge form login');
+        console.log(formValue);
+        this.setState({
+            loginData: formValue
+        });
     }
+
     image = require('../../../assets/img/5-tenedores-letras-icono-logo.png')
 
     render() {
-        const { loginStruct, loginOptions } = this.state;
+        const { loginStruct, loginOptions, loginErrorMessage } = this.state;
         return(
             <View style={styles.viewBody}>
                 <Image
@@ -33,9 +84,24 @@ export default class Login extends Component {
                         ref="loginForm"
                         type={loginStruct}
                         options={loginOptions}
+                        value={this.state.loginData}
+                        onChange={(formValue) => this.onChangeFormLogin(formValue)}
                     />
-                    <Button buttonStyle={styles.buttonLoginContainer} title="Login"/>
+                    <Button 
+                        buttonStyle={styles.buttonLoginContainer} 
+                        title="Login"
+                        onPress={() => this.login()}/>
                 </View>
+
+                <Text style={styles.loginErrorMessage}>{loginErrorMessage}</Text>
+                <Toast
+                    ref='toast'
+                    position='bottom'
+                    positionValue={180}
+                    fadeInDuration={750}
+                    fadeOutDuration={1000}
+                    opacity={0.8}
+                    textStyle={{color:'#fff'}} />
             </View>
         );
     }
@@ -64,5 +130,10 @@ const styles = StyleSheet.create({
         marginTop: 20,
         marginLeft: 10,
         marginRight: 10
+    },
+    loginErrorMessage: {
+        color: '#f00',
+        textAlign: 'center',
+        marginTop: 20
     }
 })
--- app/screens/MyAccount/MyAccount.js
+++ app/screens/MyAccount/MyAccount.js
@@ -2,19 +2,58 @@ import React, {Component} from 'react';
 import { StyleSheet, View, Text} from 'react-native';
 import { Button } from 'react-native-elements';
 
+import * as firebase from 'firebase';
+
 export default class MyAccount extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            login: false
+        }
+    }
+
+    async componentDidMount() {
+        await firebase.auth().onAuthStateChanged(user => {
+            console.log(user);
+            if (user) {
+                this.setState({
+                    login: true
+                });
+            } else {
+                this.setState({
+                    login: false
+                });
+            }
+        });
+    }
+
     goToScreen = nameScreen => {
         this.props.navigation.navigate(nameScreen);
     }
 
+    logout = () => {
+        firebase.auth().signOut();
+    }
+
     render() {
-        return (
-            <View style={styles.viewBody}>
-                <Text>MyAccount Scsssreen. </Text>
-                <Button title="Registro" onPress={() => this.goToScreen('Register')}/>
-                <Button title="Login" onPress={() => this.goToScreen('Login')}/>
-            </View>
-        )
+        const { login } = this.state;
+        if (login) {
+            return (
+                <View style={styles.viewBody}>
+                    <Text>Estas logueado correctamente</Text>
+                    <Button title="Cerrar sesión" onPress={() => this.logout()} />
+                </View>
+            )
+        } else  {
+            return (
+                <View style={styles.viewBody}>
+                    <Text>MyAccount Scsssreen. </Text>
+                    <Button title="Registro" onPress={() => this.goToScreen('Register')}/>
+                    <Button title="Login" onPress={() => this.goToScreen('Login')}/>
+                </View>
+            )
+        }
     }
 }
 
